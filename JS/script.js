eebdb3fe23fe246a8d4cafa99f0ec79349420a89
blacdav <davidaniefoik@gmail.com>
2024-01-20 22:55:25 +0100

    first commit for the project, i'm half way into the first page of the project.

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
+// this part selects all the star, via an id and sets a class of animate-bounce to each of them.
+const star = document.querySelectorAll('#star');
+
+document.addEventListener('load', ()=> {
+    for(let i = 0; i < star.length; i++){
+        star[i].classList.add('animate-bounce');
+    }
+});
+
 // with the help of an event listener on the menu button, this part listens for a click event on the
 // menu button, an then drops down the menu/nav for mobile view.
 const menu = document.querySelector('#menu');
@@ -7,28 +16,21 @@ menu.addEventListener('click', ()=> {
 
     if(menulist.classList.contains('hidden')){
         menulist.classList.replace('hidden','flex');
-        menu.classList.add('hidden')
+        menu.classList.replace('block','hidden');
     }
     else {
-        menu.classList.remove('hidden');
+        // menu.classList.remove('hidden');
     }
 });
 
 // this is to close the menu/navbar.
-const closemenu = document.querySelector('#closemenu')
+const closemenu = document.querySelector('#closemenu');
 
 closemenu.addEventListener('click', ()=> {
     if(menulist.classList.contains('flex')){
         menulist.classList.replace('flex','hidden');
     }
     else{
-        menu.classList.remove('hidden');
+        menu.classList.replace('hidden','block');
     }
-})
-
-// this part selects all the star, via an id and sets a class of animate-bounce to each of them.
-const star = document.querySelectorAll('#star');
-
-for(let i = 0; i < star.length; i++){
-    star[i].classList.add('animate-bounce');
-}
+});
